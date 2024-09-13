5c4018cbc9bcbe711e40768fe1337152fcb9e1f7
Matt Aschmann <ma7hatter@HaP.>
2024-09-13 15:37:20 -0600

    save: qbstats

--- src/ffanalyze/ffanalyze.py
+++ src/ffanalyze/ffanalyze.py
@@ -1,8 +1,21 @@
-import json
+import pprint
+from unicodedata import name
 import pandas as pd
 
-with open('data/QBs.json') as f:
-    d = json.load(f)
-    df = pd.json_normalize(d)
-    print(df)
+def qb_stats():
+    number_cols = ['GP', 'Pts', 'PaY', 'PaTd', 'Int', 'RuAt', 'RuY', 'RuTd', 'Tar', 'Rec', 'RecY', 'RecTd', 'RetY', 'RetTd', 'TwPt', 'Fum']
+    qbs = pd.read_json('data/QBs.json')
 
+    # cleanup
+    for col in number_cols:
+        qbs[col] = pd.to_numeric(qbs[col], errors='coerce')
+        qbs[col] = qbs[col].fillna(0)
+
+    # total yards
+    qbs['TotY'] = qbs['PaY'] + qbs['RuY']
+
+    print(qbs.dtypes)
+    pprint.pp(qbs)
+
+if __name__ == '__main__':
+    qb_stats()
