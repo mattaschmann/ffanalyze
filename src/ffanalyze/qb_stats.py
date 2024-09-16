035cec3d7e55cabb83ee0cee8b7e3c6f980ec0db
Matt Aschmann <ma7hatter@HaP.>
2024-09-16 12:12:54 -0600

    save qbs

--- src/ffanalyze/qb_stats.py
+++ src/ffanalyze/qb_stats.py
@@ -1,7 +1,7 @@
-import re
-
 import pandas as pd
 from pandas.io.formats.style import StylerRenderer
+
+from . import colors
 
 from ffanalyze import format
 
@@ -27,24 +27,25 @@ def sheet(file_path: str) -> pd.DataFrame:
     result['P/Y Z'] = z_score(result['Pts/Yd'])              # Points per yard z-score
     result['Y/G Z'] = z_score(result['Yds/G'])               # Yards per game z-score
     result['P/G Z'] = z_score(result['Pts/G'])               # Points per game z-score
-    result['Zval'] = (result['Y/G Z'] + result['P/G Z']) / 2 # Z value score?
+    result['Zval'] = (result['Y/G Z'] + result['P/G Z']) / 2 # Z value score
 
     return result
 
-def style_sheet(file_path: str) -> StylerRenderer:
+def style_sheet(sheet: pd.DataFrame) -> StylerRenderer:
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
 
-    result = sheet(file_path)
-
-    styled = result.sort_values(by='Zval', ascending=False)\
-        .style\
+    styled = sheet.style\
         .map(format.highlight_owner, subset='Owner')\
         .apply(lambda col: format.color_column(col, '#fce5cd'), subset='Yds')\
         .apply(lambda col: format.color_column(col, '#d9ead3'), subset='Pts/Yd')\
         .apply(lambda col: format.color_column(col, '#d9d2e9'), subset='Yds/G')\
         .apply(lambda col: format.color_column(col, '#d0e0e3'), subset='Pts/G')\
+        .background_gradient(cmap=colors.rwg_cm, subset='P/Y Z')\
+        .background_gradient(cmap=colors.dark_rwg_cm, subset='Y/G Z')\
+        .background_gradient(cmap=colors.cwy_cm, subset='P/G Z')\
+        .background_gradient(cmap=colors.bwo_cm, subset='Zval')\
         .format(precision=2, subset=['Pts', 'Pts/Yd', 'P/Y Z', 'Pts/G', 'Y/G Z', 'P/G Z', 'Zval'])\
         .format(precision=0, subset=['Yds/G'])
 
@@ -53,8 +54,8 @@ def style_sheet(file_path: str) -> StylerRenderer:
 def z_score(s) -> float:
     return (s - s.mean()) / s.std()
 
-if __name__ == '__main__':
-    style = style_sheet('data/QBs.json')
+# if __name__ == '__main__':
+#     style = style_sheet('data/QBs.json')
     # style.data.to_excel('out/qbs.xlsx')
     # print(df.style.to_html())
     # print(df)
